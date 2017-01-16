7f33c7bfa9c68e2f24e880c7250ba3474691a615
Nobuoka Yu <nobuoka@vividcode.info>
2017-01-17 01:58:10 +0900

    Create project

---
+++ app/src/main/java/info/vividcode/sample/di/kotlin/cake/MainActivity.java
@@ -0,0 +1,13 @@
+package info.vividcode.sample.di.kotlin.cake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
